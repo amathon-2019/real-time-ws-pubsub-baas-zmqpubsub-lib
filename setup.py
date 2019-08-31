d8592f3ddb6387be03bf56d4b775ac1f20c89f34
LMC <2_minchul@naver.com>
2019-08-31 23:08:25 +0900

    fix typo in setup.py

--- setup.py
+++ setup.py
@@ -3,9 +3,9 @@
 from setuptools import setup
 
 setup(
-    name='zmp_pubsub',
+    name='zmq_pubsub',
     version='0.0.1',
-    packages=['zmp_pubsub'],
+    packages=['zmq_pubsub'],
     install_requires=[
         'aiohttp==3.5.4',
         'aioredis==1.2.0',
